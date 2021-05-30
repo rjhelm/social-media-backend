05b1cd28e34211bfc01ed3f3b949fed3bb6a33be
Ryan Helm <rjhelm@github.io>
2021-05-30 15:29:48 -0600

    added to user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -62,4 +62,17 @@ const userController = {
          })
             .catch(err => res.json(err))
     },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+         .then(dbUserData => {
+         if (!dbUserData) {
+             res.status(404).json({ message: 'No user found with this ID!' });
+             return;
+         }
+         res.json(dbUserData);
+         })
+         .catch(err => res.status(400).json(err))
+    },
+    
 }
